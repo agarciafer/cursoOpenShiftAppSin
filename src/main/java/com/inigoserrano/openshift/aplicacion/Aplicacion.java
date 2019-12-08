0ea5cdb8ad7aab4f81cb86b1d4f8e6cec83d6a8f
Alberto García Fernández <38891905+agarciafer@users.noreply.github.com>
2019-12-08 22:53:20 +0100

    Update Aplicacion.java

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -24,7 +24,7 @@ public class Aplicacion extends HttpServlet {
 
 	@Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Salida y de nuevo salida salida más veces.");
+        System.out.println("Modificada por Alberto.");
         response.getWriter().println("Salida GET mecmec menos vez");  
         response.getWriter().flush();
     }
